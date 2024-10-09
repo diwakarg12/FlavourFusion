c668b46cd459da92b62cc125fbf4b05331d66581
Diwakar Giri <diwakargiri23@gmail.com>
2024-10-09 15:24:17 +0530

    installed necessary packages, added images and setup routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,50 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
+import Contact from './pages/Contact.jsx';
+import NotFound from './pages/NotFound.jsx';
+import Layout from './Layout.jsx';
+import Recipe from './pages/Recipe.jsx';
+import Pages from './pages/Pages.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/recipe:id",
+        element: <Recipe />,
+      },
+      {
+        path: "/pages",
+        element: <Pages />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ]
+  },
+  {
+        path: "*",
+        element: <NotFound />,
+      },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],    
+        playfair: ['Playfair Display', 'serif'],
+      },
+    },
   },
   plugins: [],
 }
